df22cda0f66654e7e1c9058bac7ffca8766a57d9
Vardan <varsan2224@gmail.com>
2026-07-23 21:37:57 +0400

    products UI

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,23 @@
+import { products } from "../products"
+
 function Products(){
     return (
-        <div></div>
+        <div>
+            <div className="flex justify-center w-full">
+                <input type="text" className="bg-gray-700 w-[300px] rounded-xl text-white my-8" />
+            </div>
+            <div className="grid grid-cols-2 md:grid-cols-3">
+               {products.map((product) => {
+                return (
+                    <div className="flex justify-evenly items-center">
+                        <div className="w-[200px] h-[300px] md:w-[300px] h-[300px] mx-2 my-4">
+                        <img src={product.img} className="w- object-cover"  alt="" />
+                        </div>
+                    </div>
+                )
+               })}
+            </div>
+        </div>
     )
 }
 
---
+++ src/products.js
@@ -0,0 +1,16 @@
+import IMG_1257 from './images/IMG_1257.JPG'
+import IMG_1261 from './images/IMG_1261.JPG'
+import IMG_1298 from './images/IMG_1298.JPG'
+import IMG_1301 from './images/IMG_1301.JPG'
+import IMG_1304 from './images/IMG_1304.JPG'
+import IMG_1307 from './images/IMG_1307.JPG'
+
+
+export const products = [
+   {title:'',price:'',img:IMG_1257},
+   {title:'',price:'',img:IMG_1261},
+   {title:'',price:'',img:IMG_1298},
+   {title:'',price:'',img:IMG_1301},
+   {title:'',price:'',img:IMG_1304},
+   {title:'',price:'',img:IMG_1307},
+]
